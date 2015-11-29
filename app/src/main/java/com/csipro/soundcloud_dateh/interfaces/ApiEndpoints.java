c6d8dfa3d187fbb96d8153ee5b2c06ff80991f2e
DarthCharles <carlos.darth@gmail.com>
2015-11-29 11:35:18 -0700

    Added ApiEndpoints interface

---
+++ app/src/main/java/com/csipro/soundcloud_dateh/interfaces/ApiEndpoints.java
@@ -0,0 +1,22 @@
+package com.csipro.soundcloud_dateh.interfaces;
+
+import com.csipro.soundcloud_dateh.models.Playlist;
+import com.csipro.soundcloud_dateh.models.Track;
+
+import retrofit.Call;
+import retrofit.http.GET;
+import retrofit.http.Path;
+import retrofit.http.Query;
+
+/**
+ * Created by Charles on 29/11/15.
+ */
+public interface ApiEndpoints {
+    @GET("/tracks/{id}")
+    Call<Track> getTrack(@Path("id") String id,
+                         @Query("client_id") String client_id);
+
+    @GET("/playlists/{id}")
+    Call<Playlist> getPlaylist(@Path("id") String id,
+                               @Query("client_id") String client_id);
+}
